d37933e0e250c7143006fefd08b415f04b33270c
Alexander Yakovenko <yakovenko.berdyansk@gmail.com>
2026-02-08 23:52:18 +0300

    add path in urls

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('habits/', include("habits.urls", namespace="habits")),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('users/', include("users.urls", namespace="users")),
 ]
