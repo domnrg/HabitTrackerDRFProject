0ef8ab28a6fc9001d1210316d3b88fd98621cf49
Alexander Yakovenko <yakovenko.berdyansk@gmail.com>
2026-02-10 21:40:58 +0300

    add JWT and permissions

--- habits/urls.py
+++ habits/urls.py
@@ -1,12 +1,13 @@
 from rest_framework.routers import SimpleRouter
 
-from habits.views import HabitViewSet
+from habits.views import HabitViewSet, PublicHabitViewSet
 from habits.apps import HabitsConfig
 
 app_name = HabitsConfig.name
 
 router = SimpleRouter()
 router.register('', HabitViewSet, basename='habit')
+router.register('public-habits', PublicHabitViewSet, basename='public-habit')
 
 urlpatterns = router.urls
 
--- habits/views.py
+++ habits/views.py
@@ -1,12 +1,31 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from habits.models import Habit
+from habits.permissions import IsOwner
 from habits.serializers import HabitSerializer
 
 
 class HabitViewSet(viewsets.ModelViewSet):
-    queryset = Habit.objects.all()
     serializer_class = HabitSerializer
+    permission_classes = [IsAuthenticated, IsOwner]
+
+    def get_queryset(self):
+        return Habit.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class PublicHabitViewSet(ReadOnlyModelViewSet):
+    serializer_class = HabitSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Habit.objects.filter(is_public=True)
+
+
 
 
 
