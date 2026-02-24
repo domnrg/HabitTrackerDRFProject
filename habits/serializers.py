d9a7bc7f1e033327f8066da2d5da62fc4eb1852c
Alexander Yakovenko <yakovenko.berdyansk@gmail.com>
2026-02-24 21:01:01 +0300

    add rest of tests and coverage

--- habits/serializers.py
+++ habits/serializers.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.models import User
 from rest_framework import serializers
 
 from habits.models import Habit
@@ -13,3 +12,37 @@ class HabitSerializer(serializers.ModelSerializer):
     class Meta:
         model = Habit
         fields = '__all__'
+
+    def validate(self, data):
+
+        is_pleasant = data.get('is_pleasant')
+        reward = data.get('reward')
+        related_habit = data.get('related_habit')
+        duration = data.get('duration')
+
+        if duration and duration > 120:
+            raise serializers.ValidationError(
+                {"duration": "Время выполнения привычки не должно превышать 120 секунд."}
+            )
+
+        if reward and related_habit:
+            raise serializers.ValidationError(
+                "Нельзя одновременно указывать вознаграждение и связанную привычку."
+            )
+
+        if is_pleasant and reward:
+            raise serializers.ValidationError(
+                "Приятная привычка не может иметь вознаграждение."
+            )
+
+        if related_habit and not related_habit.is_pleasant:
+            raise serializers.ValidationError(
+                "Связанная привычка должна быть приятной."
+            )
+
+        if is_pleasant and related_habit:
+            raise serializers.ValidationError(
+                "Приятная привычка не может иметь связанную привычку."
+            )
+
+        return data
--- habits/tests.py
+++ habits/tests.py
@@ -14,6 +14,7 @@ class HabitTestCase(APITestCase):
         )
         self.client.force_authenticate(user=self.user)
 
+
     def test_create_habit(self):
         """ Тестирование создания привычки """
 
@@ -32,12 +33,14 @@ class HabitTestCase(APITestCase):
         self.assertEqual(Habit.objects.count(), 1)
         self.assertTrue(Habit.objects.all().exists())
 
+
     def test_list_habit(self):
         """ Тестирование получения списка привычек пользователя """
 
         response = self.client.get("/habits/")
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
 
     def test_retrieve_habit(self):
         """ Тестирование получения детальной информации о привычке """
@@ -57,3 +60,131 @@ class HabitTestCase(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data["action"], "Читать книгу")
         self.assertEqual(response.data["place"], "Дом")
+
+
+    def test_cannot_retrieve_foreign_habit(self):
+        """Тестирование запрета получения чужой привычки."""
+
+        other_user = User.objects.create_user(
+            username="otheruser",
+            password="1234"
+        )
+
+        habit = Habit.objects.create(
+            user=other_user,
+            place="Офис",
+            time="10:00",
+            action="Пить воду",
+            duration=30,
+            periodicity=1,
+            is_public=False
+        )
+
+        response = self.client.get(f"/habits/{habit.id}/")
+
+        self.assertIn(response.status_code, [status.HTTP_403_FORBIDDEN, status.HTTP_404_NOT_FOUND])
+
+
+    def test_update_own_habit(self):
+        """ Тестирование обновления привычки пользователя """
+
+        habit = Habit.objects.create(
+            user=self.user,
+            place="Дом",
+            time="12:00",
+            action="Старая привычка",
+            duration=60,
+            periodicity=1,
+            is_public=False
+        )
+
+        response = self.client.patch(
+            f"/habits/{habit.id}/",
+            {"action": "Новая привычка"}
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        habit.refresh_from_db()
+        self.assertEqual(habit.action, "Новая привычка")
+
+
+    def test_delete_own_habit(self):
+        """ Тестирование удаления привычки пользователя """
+
+        habit = Habit.objects.create(
+            user=self.user,
+            place="Дом",
+            time="12:00",
+            action="Удалить меня",
+            duration=60,
+            periodicity=1,
+            is_public=False
+        )
+
+        response = self.client.delete(f"/habits/{habit.id}/")
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertFalse(Habit.objects.filter(id=habit.id).exists())
+
+
+    def test_periodicity_limit_validation(self):
+        """ Тестирование ограничения периодичности привычки"""
+
+        data = {
+            "place": "Дом",
+            "time": "12:00",
+            "action": "Бег",
+            "duration": 60,
+            "periodicity": 10,  # больше 7
+            "is_public": False
+        }
+
+        response = self.client.post("/habits/", data)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+    def test_duration_limit_validation(self):
+        """ Тестирование ограничения длительности привычки """
+
+        data = {
+            "place": "Дом",
+            "time": "12:00",
+            "action": "Читать",
+            "duration": 150,  # больше 120
+            "periodicity": 1,
+            "is_public": False
+        }
+
+        response = self.client.post("/habits/", data)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+    def test_reward_and_related_habit_validation(self):
+        """ Тестирование запрета одновременного указания вознаграждения и связанной привычки """
+
+        related_habit = Habit.objects.create(
+            user=self.user,
+            place="Дом",
+            time="10:00",
+            action="Базовая привычка",
+            duration=60,
+            periodicity=1,
+            is_public=False
+        )
+
+        data = {
+            "place": "Дом",
+            "time": "12:00",
+            "action": "Новая привычка",
+            "duration": 60,
+            "periodicity": 1,
+            "reward": "Шоколад",
+            "related_habit": related_habit.id,
+            "is_public": False
+        }
+
+        response = self.client.post("/habits/", data)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
